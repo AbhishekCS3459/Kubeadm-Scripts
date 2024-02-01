d7c827f3c97b222208cf13ae2537fd195d59ad1c
Abhishek <abhishekverman3459@gmail.com>
2024-02-01 20:35:32 +0530

    adding echo to common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 #
-# Common setup for all servers (Control Plane and Nodes)
-
+echo "# Common setup for all servers (Control Plane and Nodes)"
 set -euxo pipefail
 
-# Variable Declaration
+echo "# Variable Declaration"
 
 KUBERNETES_VERSION="1.28.1-00"
 
-# disable swap
+echo "# disable swap"
 sudo swapoff -a
 
-# keeps the swaf off during reboot
+echo "# keeps the swaf off during reboot"
 (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
 sudo apt-get update -y
 
 
-# Install CRI-O Runtime
+echo "# Install CRI-O Runtime"
 
 OS="xUbuntu_22.04"
 
 VERSION="1.28"
 
-# Create the .conf file to load the modules at bootup
+echo "# Create the .conf file to load the modules at bootup"
 cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
 overlay
 br_netfilter
 EOF
 
+echo "modprobe overlay"
 sudo modprobe overlay
+echo "moprobe br_netfilter"
 sudo modprobe br_netfilter
 
 # Set up required sysctl params, these persist across reboots.
+echo "sysctl net.bridge.bridge-nf-call-iptables"
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
+echo "sysctl --system"
 sudo sysctl --system
 
+echo "installing the crio runtime"
 cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
 EOF
@@ -47,12 +51,15 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cr
 deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
 EOF
 
+echo "adding the key"
 curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
 
+echo "updating the repo"
 sudo apt-get update
 sudo apt-get install cri-o cri-o-runc -y
 
+echo  "creating the crio.conf file"
 sudo systemctl daemon-reload
 sudo systemctl enable crio --now
 
